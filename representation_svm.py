1860820ba4bd3c89dc469cc9fa3769f6746ba62b
Bruno de Araujo Alves <me@baraus.dev>
2021-12-17 02:25:36 -0300

    multiprocessing

--- representation_svm.py
+++ representation_svm.py
@@ -1,8 +1,10 @@
 import argparse
+import multiprocessing
 import operator
 
 import librosa.display
 import numpy as np
+from numpy.core.fromnumeric import repeat
 import pandas as pd
 import scipy.io as sio
 from sklearn.metrics import f1_score
@@ -42,7 +44,6 @@ params = {
     # 'feature': 'lpc'
 }
 
-print(params)
 
 datatable = {
     'feature': [],
@@ -65,147 +66,155 @@ if False:
     augmentation = 2
     noise = 0
 
-for segment in params['segment_time']:
-    print('[SEG] ', segment)
-    mat_audios = sio.loadmat(f'datasets/ifgaudioData{segment}seg.mat')
-    mat_audios = mat_audios['ifgaudioData']
+def process_mfcc(arg):
+    rep = librosa.feature.mfcc(arg[0], fs, n_mfcc=arg[1])
+    return rep.flatten()
 
-    data_audio = mat_audios['Data'][0, 0]
-    labels_audio = mat_audios['Labels'][0, 0]
+def process_lpc(arg):
+    return librosa.lpc(arg[0], order=arg[1])
 
-    # remove row that only contains zero
-    rows = np.all(data_audio == 0, axis=1)
-    data_audio = data_audio[~rows]
-    labels_audio = labels_audio[~rows]
+def process_stft(arg):
+    rep = np.abs(librosa.stft(arg[0], n_fft=arg[1]))
+    return rep.flatten()
 
-    labels = []
+if __name__ == '__main__':   
+    print(params)
 
-    for label in labels_audio[:, 0]:
-        labels.append(label[0, 0])
+    for segment in params['segment_time']:
+        print('[SEG] ', segment)
+        mat_audios = sio.loadmat(f'datasets/ifgaudioData{segment}seg.mat')
+        mat_audios = mat_audios['ifgaudioData']
 
-    labels_audio = np.array(labels)
+        data_audio = mat_audios['Data'][0, 0]
+        labels_audio = mat_audios['Labels'][0, 0]
 
-    X_train, X_test, y_train, y_test = train_test_split(data_audio,
-                                                        labels_audio,
-                                                        stratify=labels_audio,
-                                                        test_size=0.2,
-                                                        random_state=42)
+        # remove row that only contains zero
+        rows = np.all(data_audio == 0, axis=1)
+        data_audio = data_audio[~rows]
+        labels_audio = labels_audio[~rows]
 
-    for augmentation in params['augmentation']:
-        print('[AUG] ', augmentation)
-        X_train_aug = np.array(X_train.tolist() * augmentation)
-        y_train_aug = np.array(y_train.tolist() * augmentation)
+        labels = []
 
-        for noise in params['noise']:
-            print('[NOISE] ', noise)
-            sinal = X_train[0, :]
-            numAmostras = sinal.size
+        for label in labels_audio[:, 0]:
+            labels.append(label[0, 0])
 
-            if noise != 0:
-                SNR = Math.mag2db(noise)
-                potSinal = Math.rms(sinal) ** 2
-                potRuido = 1 / potSinal
-                ruidoAditivo = np.random.randn(numAmostras) * np.std(sinal) / Math.db2mag(SNR)
-            else:
-                ruidoAditivo = np.zeros(numAmostras)
+        labels_audio = np.array(labels)
 
-            for audio_segment in range(X_train_aug.shape[0]):
-                X_train_aug[audio_segment] = X_train_aug[audio_segment] + ruidoAditivo
+        X_train, X_test, y_train, y_test = train_test_split(data_audio,
+                                                            labels_audio,
+                                                            stratify=labels_audio,
+                                                            test_size=0.2,
+                                                            random_state=42)
 
-            X_train_aug = np.concatenate((X_train, X_train_aug), axis=0)
-            y_train_aug = np.concatenate((y_train, y_train_aug), axis=0)
+        for augmentation in params['augmentation']:
+            print('[AUG] ', augmentation)
+            X_train_aug = np.array(X_train.tolist() * augmentation)
+            y_train_aug = np.array(y_train.tolist() * augmentation)
 
-            for coeff in params['coeffs']:
-                print(f'[{params["feature"].upper()}] ', coeff)
-                X_train_rep = None
-                X_test_rep = None
-                rep_shape = None
+            for noise in params['noise']:
+                print('[NOISE] ', noise)
+                sinal = X_train[0, :]
+                numAmostras = sinal.size
 
-                if params['feature'] == 'mfcc':
-                    rep_size = librosa.feature.mfcc(X_train_aug[0], fs, n_mfcc=coeff)
-                    rep_shape = rep_size.shape
-                    X_train_rep = np.zeros((X_train_aug.shape[0], rep_size.size))
-                    X_test_rep = np.zeros((X_test.shape[0], rep_size.size))
+                if noise != 0:
+                    SNR = Math.mag2db(noise)
+                    potSinal = Math.rms(sinal) ** 2
+                    potRuido = 1 / potSinal
+                    ruidoAditivo = np.random.randn(numAmostras) * np.std(sinal) / Math.db2mag(SNR)
+                else:
+                    ruidoAditivo = np.zeros(numAmostras)
 
-                    for i in range(X_train_aug.shape[0]):
-                        rep = librosa.feature.mfcc(X_train_aug[i], fs, n_mfcc=coeff)
-                        X_train_rep[i] = rep.flatten()
+                for audio_segment in range(X_train_aug.shape[0]):
+                    X_train_aug[audio_segment] = X_train_aug[audio_segment] + ruidoAditivo
 
-                    for i in range(X_test.shape[0]):
-                        rep = librosa.feature.mfcc(X_test[i], fs, n_mfcc=coeff)
-                        X_test_rep[i] = rep.flatten()
+                X_train_aug = np.concatenate((X_train, X_train_aug), axis=0)
+                y_train_aug = np.concatenate((y_train, y_train_aug), axis=0)
 
-                if params['feature'] == 'stft':
-                    rep_size = np.abs(librosa.stft(X_train_aug[0], n_fft=coeff))
-                    rep_shape = rep_size.shape
-                    X_train_rep = np.zeros((X_train_aug.shape[0], rep_size.size))
-                    X_test_rep = np.zeros((X_test.shape[0], rep_size.size))
+                for coeff in params['coeffs']:
+                    print(f'[{params["feature"].upper()}] ', coeff)
+                    X_train_rep = None
+                    X_test_rep = None
+                    rep_shape = None
 
-                    for i in range(X_train_aug.shape[0]):
-                        rep = np.abs(librosa.stft(X_train_aug[i], n_fft=coeff))
-                        X_train_rep[i] = rep.flatten()
-
-                    for i in range(X_test.shape[0]):
-                        rep = np.abs(librosa.stft(X_test[i], n_fft=coeff))
-                        X_test_rep[i] = rep.flatten()
-
-                if params['feature'] == 'lpc':
-                    rep_size = librosa.lpc(X_train_aug[0], order=coeff)
-                    rep_shape = rep_size.shape
-                    X_train_rep = np.zeros((X_train_aug.shape[0], rep_size.size))
-                    X_test_rep = np.zeros((X_test.shape[0], rep_size.size))
-
-                    for i in range(X_train_aug.shape[0]):
-                        rep = librosa.lpc(X_train_aug[i], order=coeff)
-                        X_train_rep[i] = rep.flatten()
-
-                    for i in range(X_test.shape[0]):
-                        rep = librosa.lpc(X_test[i], order=coeff)
-                        X_test_rep[i] = rep.flatten()
-
-                se = StandardScaler()
-                le = LabelEncoder()
-
-                X_train_rep = se.fit_transform(
-                    X_train_rep.reshape(-1, X_train_rep.shape[-1])).reshape(X_train_rep.shape)
-                X_test_rep = se.transform(
-                    X_test_rep.reshape(-1, X_test_rep.shape[-1])).reshape(X_test_rep.shape)
-
-                y_train_rep = le.fit_transform(y_train_aug)
-                y_test_rep = le.transform(y_test)
-
-                # TRAIN
-                print('[TRAIN] starting')
-                clf = SVC(kernel='linear', C=10)
-                scoring = ['precision_macro', 'recall_macro', 'f1_macro', 'f1_micro']
-                scores = cross_validate(clf, X_train_rep, y_train_rep,
-                                        scoring=scoring, cv=3,
-                                        return_estimator=True,
-                                        return_train_score=True,
-                                        n_jobs=-1)
-
-                # PREDICT
-                print('[PREDICT] starting')
-                idx_estimator, _ = max(enumerate(scores['test_f1_micro']), key=operator.itemgetter(1))
-                estimator = scores['estimator'][idx_estimator]
-                y_hat = estimator.predict(X_test_rep)
-                fmacro = f1_score(y_test_rep, y_hat, average='macro')
-                fmicro = f1_score(y_test_rep, y_hat, average='micro')
-
-                print('[DATA] saving')
-                datatable['feature'].append(params['feature'])
-                datatable['feature_coeff'].append(coeff)
-                datatable['segment_time'].append(segment)
-                datatable['noise_percentage'].append(noise)
-                datatable['augmentation'].append(augmentation)
-                datatable['f1_micro'].append(fmicro)
-                datatable['f1_macro'].append(fmacro)
-                datatable['train_size'].append(X_train.shape)
-                datatable['test_size'].append(X_test.shape)
-                datatable['representation_size'].append(rep_shape)
+                    pool_obj = multiprocessing.Pool()
 
 
-                df = pd.DataFrame(datatable)
-                df.to_csv(f'results/{params["feature"]}_svm.csv')
+                    if params['feature'] == 'mfcc':
+                        rep_size = librosa.feature.mfcc(X_train_aug[0], fs, n_mfcc=coeff)
+                        rep_shape = rep_size.shape
+                        X_train_rep = np.zeros((X_train_aug.shape[0], rep_size.size))
+                        X_test_rep = np.zeros((X_test.shape[0], rep_size.size))
 
-                del X_train_rep, X_test_rep, y_train_rep, y_test_rep
+                        X_train_rep = pool_obj.map(process_mfcc,zip(X_train_aug, repeat(coeff,X_train_aug.shape[0])))
+                        X_train_rep = np.array(X_train_rep)
+                        X_test_rep = pool_obj.map(process_mfcc,zip(X_test, repeat(coeff,X_train_aug.shape[0])))
+                        X_test_rep = np.array(X_test_rep)
+
+                    if params['feature'] == 'stft':
+                        rep_size = np.abs(librosa.stft(X_train_aug[0], n_fft=coeff))
+                        rep_shape = rep_size.shape
+                        X_train_rep = np.zeros((X_train_aug.shape[0], rep_size.size))
+                        X_test_rep = np.zeros((X_test.shape[0], rep_size.size))
+
+                        X_train_rep = pool_obj.map(process_stft,zip(X_train_aug, repeat(coeff,X_train_aug.shape[0])))
+                        X_train_rep = np.array(X_train_rep)
+                        X_test_rep = pool_obj.map(process_stft,zip(X_test, repeat(coeff,X_train_aug.shape[0])))
+                        X_test_rep = np.array(X_test_rep)
+
+                    if params['feature'] == 'lpc':
+                        rep_size = librosa.lpc(X_train_aug[0], order=coeff)
+                        rep_shape = rep_size.shape
+                        X_train_rep = np.zeros((X_train_aug.shape[0], rep_size.size))
+                        X_test_rep = np.zeros((X_test.shape[0], rep_size.size))
+
+                        X_train_rep = pool_obj.map(process_lpc,zip(X_train_aug, repeat(coeff,X_train_aug.shape[0])))
+                        X_train_rep = np.array(X_train_rep)
+                        X_test_rep = pool_obj.map(process_lpc,zip(X_test, repeat(coeff,X_train_aug.shape[0])))
+                        X_test_rep = np.array(X_test_rep)
+
+                    se = StandardScaler()
+                    le = LabelEncoder()
+
+                    X_train_rep = se.fit_transform(
+                        X_train_rep.reshape(-1, X_train_rep.shape[-1])).reshape(X_train_rep.shape)
+                    X_test_rep = se.transform(
+                        X_test_rep.reshape(-1, X_test_rep.shape[-1])).reshape(X_test_rep.shape)
+
+                    y_train_rep = le.fit_transform(y_train_aug)
+                    y_test_rep = le.transform(y_test)
+
+                    # TRAIN
+                    print('[TRAIN] starting')
+                    clf = SVC(kernel='linear', C=10)
+                    scoring = ['precision_macro', 'recall_macro', 'f1_macro', 'f1_micro']
+                    scores = cross_validate(clf, X_train_rep, y_train_rep,
+                                            scoring=scoring, cv=3,
+                                            return_estimator=True,
+                                            return_train_score=True,
+                                            n_jobs=-1)
+
+                    # PREDICT
+                    print('[PREDICT] starting')
+                    idx_estimator, _ = max(enumerate(scores['test_f1_micro']), key=operator.itemgetter(1))
+                    estimator = scores['estimator'][idx_estimator]
+                    y_hat = estimator.predict(X_test_rep)
+                    fmacro = f1_score(y_test_rep, y_hat, average='macro')
+                    fmicro = f1_score(y_test_rep, y_hat, average='micro')
+
+                    print('[DATA] saving')
+                    datatable['feature'].append(params['feature'])
+                    datatable['feature_coeff'].append(coeff)
+                    datatable['segment_time'].append(segment)
+                    datatable['noise_percentage'].append(noise)
+                    datatable['augmentation'].append(augmentation)
+                    datatable['f1_micro'].append(fmicro)
+                    datatable['f1_macro'].append(fmacro)
+                    datatable['train_size'].append(X_train.shape)
+                    datatable['test_size'].append(X_test.shape)
+                    datatable['representation_size'].append(rep_shape)
+
+
+                    df = pd.DataFrame(datatable)
+                    df.to_csv(f'results/{params["feature"]}_svm.csv')
+
+                    del X_train_rep, X_test_rep, y_train_rep, y_test_rep
